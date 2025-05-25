af6d637e13a65bbacc09de4ad578bf5337fc29bf
AltcoinDaddy <affiakalu@gmail.com>
2025-05-25 10:28:18 +0100

    Token contact address updated

--- src/constants/contract.ts
+++ src/constants/contract.ts
@@ -5,7 +5,7 @@ import { createWallet, inAppWallet } from "thirdweb/wallets";
 
 export const chilizChainId = 88888;
 export const contractAddress = "0xB6004245A8a2B6D8F4b68937cDc069C6a8Fc2f40";
-export const tokenAddress = "0x39fa5ab558799b2636fd110e10cfa93111ad6f1b";
+export const tokenAddress = "0xBd5bABA6EB9591e12dfBb8C044b177832B1E6DB0";
 
 export const contract = getContract({
   client: client,
